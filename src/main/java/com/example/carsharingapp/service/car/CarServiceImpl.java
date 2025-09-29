8e809790560c2bf829695fface08cc40735892b2
DariaBakal <bakal.dasha01@gmail.com>
2025-09-29 18:43:46 +0300

    added car controller and related logic

---
+++ src/main/java/com/example/carsharingapp/service/car/CarServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.carsharingapp.service.car;
+
+import com.example.carsharingapp.dto.car.AddCarRequestDto;
+import com.example.carsharingapp.dto.car.CarDto;
+import com.example.carsharingapp.dto.car.UpdateCarRequestDto;
+import com.example.carsharingapp.exception.EntityNotFoundException;
+import com.example.carsharingapp.mapper.CarMapper;
+import com.example.carsharingapp.model.Car;
+import com.example.carsharingapp.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CarServiceImpl implements CarService {
+    private final CarRepository carRepository;
+    private final CarMapper carMapper;
+
+    @Override
+    public CarDto addCar(AddCarRequestDto requestDto) {
+        Car car = carMapper.toModel(requestDto);
+        carRepository.save(car);
+        return carMapper.toDto(car);
+    }
+
+    @Override
+    public Page<CarDto> findAll(Pageable pageable) {
+        return carRepository.findAll(pageable)
+                .map(carMapper::toDto);
+    }
+
+    @Override
+    public CarDto findById(Long id) {
+        return carRepository.findById(id)
+                .map(carMapper::toDto)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("Can't find car with id: " + id));
+    }
+
+    @Override
+    public CarDto updateCar(Long id, UpdateCarRequestDto requestDto) {
+        Car car = carRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find car with id: " + id));
+        carMapper.updateCarFromDto(requestDto, car);
+        carRepository.save(car);
+        return carMapper.toDto(car);
+    }
+
+    @Override
+    public void delete(Long id) {
+        carRepository.deleteById(id);
+    }
+}
