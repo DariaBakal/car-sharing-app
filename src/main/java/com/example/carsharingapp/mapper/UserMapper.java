444105aa225d94ff8694c25882f25240af5e7211
DariaBakal <bakal.dasha01@gmail.com>
2025-09-29 17:28:50 +0300

    added user controller and related logic

--- src/main/java/com/example/carsharingapp/mapper/UserMapper.java
+++ src/main/java/com/example/carsharingapp/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package com.example.carsharingapp.mapper;
 
 import com.example.carsharingapp.config.MapperConfig;
-import com.example.carsharingapp.dto.UserRegistrationRequestDto;
-import com.example.carsharingapp.dto.UserRegistrationResponseDto;
+import com.example.carsharingapp.dto.user.UpdateUserProfileRequestDto;
+import com.example.carsharingapp.dto.user.UpdateUserRoleRequestDto;
+import com.example.carsharingapp.dto.user.UserDto;
+import com.example.carsharingapp.dto.user.UserRegistrationRequestDto;
+import com.example.carsharingapp.dto.user.UserRegistrationResponseDto;
 import com.example.carsharingapp.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
@@ -13,5 +17,15 @@ public interface UserMapper {
     @Mapping(target = "deleted", ignore = true)
     User toModel(UserRegistrationRequestDto requestDto);
 
-    UserRegistrationResponseDto toDto(User user);
+    UserDto toDto(User user);
+
+    UserRegistrationResponseDto toRegistrationResponseDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    void updateRoleFromDto(UpdateUserRoleRequestDto requestDto, @MappingTarget User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    void updateUserProfileFromDto(UpdateUserProfileRequestDto requestDto, @MappingTarget User user);
 }
--- src/main/java/com/example/carsharingapp/service/UserServiceImpl.java
+++ src/main/java/com/example/carsharingapp/service/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.example.carsharingapp.service;
 
-import com.example.carsharingapp.dto.UserRegistrationRequestDto;
-import com.example.carsharingapp.dto.UserRegistrationResponseDto;
+import com.example.carsharingapp.dto.user.UpdateUserProfileRequestDto;
+import com.example.carsharingapp.dto.user.UpdateUserRoleRequestDto;
+import com.example.carsharingapp.dto.user.UserDto;
+import com.example.carsharingapp.dto.user.UserRegistrationRequestDto;
+import com.example.carsharingapp.dto.user.UserRegistrationResponseDto;
+import com.example.carsharingapp.exception.EntityNotFoundException;
 import com.example.carsharingapp.exception.RegistrationException;
 import com.example.carsharingapp.mapper.UserMapper;
 import com.example.carsharingapp.model.User;
@@ -29,6 +33,31 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         user.setRole(Role.CUSTOMER);
         userRepository.save(user);
+        return userMapper.toRegistrationResponseDto(user);
+    }
+
+    @Override
+    public UserDto updateUserRole(Long userId, UpdateUserRoleRequestDto requestDto) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new EntityNotFoundException("Can't find user with id: " + userId));
+        userMapper.updateRoleFromDto(requestDto, user);
+        userRepository.save(user);
         return userMapper.toDto(user);
+    }
+
+    @Override
+    public UserDto getMyProfileInfo(User user) {
+        User userFromDb = userRepository.findById(user.getId()).orElseThrow(
+                () -> new EntityNotFoundException("Can't find user with id: " + user.getId()));
+        return userMapper.toDto(userFromDb);
+    }
+
+    @Override
+    public UserDto updateUserProfile(User user, UpdateUserProfileRequestDto requestDto) {
+        User userFromDb = userRepository.findById(user.getId()).orElseThrow(
+                () -> new EntityNotFoundException("Can't find user with id: " + user.getId()));
+        userMapper.updateUserProfileFromDto(requestDto, userFromDb);
+        userRepository.save(userFromDb);
+        return userMapper.toDto(userFromDb);
     }
 }
