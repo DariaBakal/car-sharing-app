df5c1e32cd0cfa1f2f447a278650cecfa61cee4f
DariaBakal <bakal.dasha01@gmail.com>
2025-10-20 21:43:03 +0200

    added tests; bugfix

--- src/main/java/com/example/carsharingapp/dto/user/UpdateUserRoleRequestDto.java
+++ src/main/java/com/example/carsharingapp/dto/user/UpdateUserRoleRequestDto.java
@@ -3,8 +3,10 @@ package com.example.carsharingapp.dto.user;
 import com.example.carsharingapp.model.User.Role;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class UpdateUserRoleRequestDto {
     @NotNull
     private Role role;
